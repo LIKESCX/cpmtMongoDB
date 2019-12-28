a3449e9f3a88446e98b4c45065cf28e852ace4f3
admin <shangchuanxiang@126.com>
2019-12-28 21:46:43 +0800

    首次提交 by me

---
+++ src/main/java/com/example/demo/main/CpmtMongoDbApplication.java
@@ -0,0 +1,22 @@
+package com.example.demo.main;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.Banner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+@SpringBootApplication
+@EnableDiscoveryClient
+public class CpmtMongoDbApplication {
+	private final static Logger logger = LoggerFactory.getLogger(CpmtMongoDbApplication.class);
+	public static void main(String[] args) {
+		System.setProperty("spring.devtools.restart.enabled", "true");//开启热部署
+		SpringApplication app = new SpringApplication(CpmtMongoDbApplication.class);
+		app.setBannerMode(Banner.Mode.OFF);//去掉打印spring图标
+		app.run(args);
+		logger.info("start cpmt-mongodb....");
+	}
+
+}
